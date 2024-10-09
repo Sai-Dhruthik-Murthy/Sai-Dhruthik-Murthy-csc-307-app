c401b4ae922ac920825435ada0f5c23e76e4e317
Sai <saimurthy66@gmail.com>
2024-10-08 22:20:15 -0700

    done

--- packages/react-frontend/src/Form.jsx
+++ packages/react-frontend/src/Form.jsx
@@ -3,14 +3,12 @@ import React, { useState } from "react";
 function Form() {
   const [person, setPerson] = useState({
     name: "",
-    job: ""
+    job: "",
   });
-
 
   function handleChange(event) {
     const { name, value } = event.target;
-    if (name === "job")
-        setPerson({ name: person["name"], job: value });
+    if (name === "job") setPerson({ name: person["name"], job: value });
     else setPerson({ name: value, job: person["job"] });
   }
 
