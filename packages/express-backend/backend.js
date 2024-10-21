a8d000f85a47369b5a3fc96ecf97bc3a57b35109
Sai <saimurthy66@gmail.com>
2024-10-21 13:32:19 -0700

    Done with IE4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -20,18 +20,45 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/users', (req, res) => {
-  const { name, job } = req.query;
-  console.log('Fetching users with query:', { name, job });
-  userService.getUsers(name, job)
-    .then(users => {
-      console.log('Users fetched:', users);
-      res.status(200).json(users);
-    })
-    .catch(error => {
-      console.error('Error fetching users:', error);
-      res.status(500).json(error);
-    });
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  const job = req.query.job;
+
+  if (name !== undefined && job !== undefined) {
+    userService
+      .findUserByNameAndJob(name, job)
+      .then((result) => {
+        userService.findUserByJob(job)
+          res.send({ users_list: result });
+        })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send("Error finding users by name and job")
+      });
+  } else if (name !== undefined) {
+    userService
+      .findUserByName(name)
+      .then((result) => {
+        res.send({ users_list: result });
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send("Error finding users by name")
+      });
+  } else if (job !== undefined) {
+    userService.findUserByJob(job)
+      .then((result) => {
+        res.send({ users_list: result });
+      })
+      .catch((error) => res.status(500).send("Error finding users by job"));
+  } else {
+    userService
+      .getUsers()
+      .then((result) => {
+        res.send({ users_list: result });
+      })
+      .catch((error) => res.status(500).send("Error fetching all users"));
+  }
 });
 
 app.get('/users/:id', (req, res) => {
